74413b80f1eda3a764d611cf739b446903b040b0
sergeialimov <8912652@gmail.com>
2020-10-19 13:15:19 +0300

    clean up boilerplate

--- src/index.js
+++ src/index.js
@@ -3,9 +3,4 @@ import React from "react";
 import ReactDOM from "react-dom";
 import App from './App.tsx';
 
-const Index = () => {
-  return <div>Hello React! You are ready for heroku</div>;
-};
-
 ReactDOM.render(<App />, document.getElementById("index"));
-// ReactDOM.render(<Index />, document.getElementById("index"));
