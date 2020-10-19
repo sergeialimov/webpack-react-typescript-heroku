0c22bf84409eae1e4a972ca1136547ed6bef1ff7
sergeialimov <8912652@gmail.com>
2020-10-19 16:45:45 +0300

    add newUser page

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,14 @@
 import React from "react";
-import { BrowserRouter as Router, Route } from 'react-router-dom';
+import { BrowserRouter as Router, Route, Link } from 'react-router-dom';
 import Home from './scenes/Home';
+import NewUser from './scenes/New-user';
 
 
 function App() {
   return (
     <Router>
       <Route path="/" exact component={Home} />
+      <Route path="/api/exercise/new-user" exact component={NewUser} />
         {/* <Route path="/products/:id" component={Product} /> */}
     </Router>
   );
