452851bb4a3f2216d1fff76217a203d7a94c8bbc
sergeialimov <8912652@gmail.com>
2020-11-23 19:43:54 +0300

    clean up indec

--- src/index.js
+++ src/index.js
@@ -1,47 +1,14 @@
-import { createStore, combineReducers } from 'redux';
-// import theReducer from './reducers.js';
-import createReducers from './reducers.js';
-
-
-const theReducer = (state = {}, action) => {
-  switch (action.type) {
-    case 'PUT_USER_DATA':
-      return action.userData;
-    default:
-      return state;
-  }
-};
-
-export function configureStore(initialState = {}) {
-  // console.log('-- -- -- reducers', reducers);
-
-  const store = createStore(
-    theReducer,
-    initialState,
-    window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__(),
-  );
-  console.log('-- -- -- store0', store);
-
-  return store;
-}
-
-export const store = configureStore();
-
-
-
 import React from "react";
 import ReactDOM from "react-dom";
 import { Provider } from 'react-redux';
-import App from './App.tsx';
-// import store from './store.js';
 
-// const store = configureStore();
+import AppContainer from './AppContainer.js';
+import { store } from './store.js';
 
-console.log('-- -- -- store', store);
 
 ReactDOM.render(
   <Provider store={store}>
-    <App />,
+    <AppContainer />
   </Provider>,
   document.getElementById("index")
 );
