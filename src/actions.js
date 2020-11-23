9d747a69a633aabef8312561a64126589d1fbca9
sergeialimov <8912652@gmail.com>
2020-11-23 19:41:57 +0300

    convert actions to ts

--- src/actions.js
+++
@@ -1,4 +0,0 @@
-export const putUserDataInStore = userData => ({
-  type: 'PUT_USER_DATA',
-  userData,
-});
---
+++ src/actions.ts
@@ -0,0 +1,4 @@
+export const putUserDataInStore = (userData: string) => ({
+  type: 'PUT_USER_DATA',
+  userData,
+});
