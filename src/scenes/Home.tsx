a8f9baafce243d34f8821b221699721ab704c643
sergeialimov <8912652@gmail.com>
2020-11-25 13:15:29 +0300

    move NewExerciseForm from Home

--- src/scenes/Home.tsx
+++ src/scenes/Home.tsx
@@ -1,5 +1,5 @@
 import React, { Component } from 'react';
-import { NewUserForm } from './components';
+import { NewUserForm, NewExerciseForm } from './components';
 
 // const UserDispatch = React.createContext(null);
 
@@ -25,15 +25,8 @@ class Home extends Component<HomeProps & HomeDispatchProps> {
 
             <NewUserForm />
 
-            <form action="/api/exercise/add" method="post">
-              <h3>Add exercises</h3>
-              <p><code>POST /api/exercise/add</code></p>
-              <input id="uid" type="text" name="userId" placeholder="userId*" />
-              <input id="desc" type="text" name="description" placeholder="description*" />
-              <input id="dur" type="text" name="duration" placeholder="duration* (mins.)" />
-              <input id="dat" type="text" name="date" placeholder="date (yyyy-mm-dd)" />
-              <input type="submit" value="Submit" />
-            </form>
+            <NewExerciseForm />
+
             <p>
               <strong>GET users's exercise log: </strong>
               <code>GET /api/exercise/log?{'userId'}[&amp;from][&amp;to][&amp;limit]`</code>
--- src/scenes/components/index.ts
+++ src/scenes/components/index.ts
@@ -1,1 +1,2 @@
 export { NewUserForm as NewUserForm } from './New-user-form';
+export { NewExerciseForm as NewExerciseForm } from './New-exercise-form';
