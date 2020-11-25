92e8cc32b6447a86e7a4e49a8269b9eb95b87938
sergeialimov <8912652@gmail.com>
2020-11-25 13:41:36 +0300

    clean up container

--- src/AppContainer.ts
+++ src/AppContainer.ts
@@ -13,24 +13,9 @@ const mapDispatchToProps = {
   putUserDataInStore,
 };
 
-// const mapDispatchToProps = (dispatch: Dispatch<Types.RootAction>) => ({
-//   handleClick: () => dispatch(putUserDataInStore())
-//   // handleClick: () => dispatch(incrementCounterAction())
-// });
-
 const AppContainer = connect(
   mapStateToProps,
   mapDispatchToProps
 )(App);
 
 export default AppContainer;
-
-
-// function mapDispatchToProps(dispatch: Redux.Dispatch<any>, ownProps: OwnProps): DispatchProps {
-//   ...
-// }
-
-// function mapDispatchToProps(dispatch: Dispatch<any>) {
-//   return dispatch(putUserDataInStore);
-//   // return dispatch({ type: 'ALERT_USER' });
-// }
