ca1937242a0bc073f23ecf016472013194470d02
sergeialimov <8912652@gmail.com>
2020-11-26 15:45:12 +0300

    add store dir

--- src/store/store.ts
+++ src/store/store.ts
@@ -1,5 +1,5 @@
 import { createStore, applyMiddleware } from 'redux';
-import { userReducer } from './reducers';
+import { userReducer } from '../reducers';
 import { composeWithDevTools } from 'redux-devtools-extension';
 
 
