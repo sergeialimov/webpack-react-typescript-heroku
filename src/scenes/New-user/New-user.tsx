5ffa0b5e0d5a7f9e4ea099b7c53c353ab7e24a38
sergeialimov <8912652@gmail.com>
2020-12-03 16:55:37 +0300

    update format of user info

--- src/scenes/New-user/New-user.tsx
+++ src/scenes/New-user/New-user.tsx
@@ -4,9 +4,9 @@ import { useStore } from 'react-redux';
 export const NewUser = () => {
   const store = useStore();
   const state = store.getState();
-  console.log(state.username);
+  const userData = `{"username":"${state.username}","_id":""}`;
 
   return (
-    <div>{state.username}</div>
+    <div>{userData}</div>
   )
 }
