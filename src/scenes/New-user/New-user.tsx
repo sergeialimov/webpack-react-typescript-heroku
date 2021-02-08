c64f872c8bf0b0ef6a3a58fbec7712ce4034fddd
sergeialimov <8912652@gmail.com>
2021-02-08 13:23:58 +0300

    add mapStateToProps

--- src/scenes/New-user/New-user.tsx
+++ src/scenes/New-user/New-user.tsx
@@ -1,13 +1,19 @@
 import React from "react";
-import { useStore } from 'react-redux';
+import { connect } from 'react-redux';
 
-export const NewUser = () => {
-  const store = useStore();
-  const state = store.getState();
-  const { user } = state;
+const mapStateToProps = (state: any) => ({ user: state.user });
+
+
+const NewUser = (props: any) => {
+  const { user } = props;
   const userData = `{"username":"${user.name}","_id":"${user.id}"}`;
+  console.log('------', props.user);
 
   return (
     <div>{userData}</div>
   )
 }
+
+export default connect(
+  mapStateToProps,
+)(NewUser);
