cde0c8e91086437bd7410dae0561d1dfb4e3b2c5
sergeialimov <8912652@gmail.com>
2021-02-10 15:49:15 +0300

    add users route

--- src/App-router.tsx
+++ src/App-router.tsx
@@ -1,13 +1,14 @@
 import React from "react";
 import { BrowserRouter as Router, Route } from 'react-router-dom';
-import { Home, NewUser } from './scenes';
+import { Home, NewUser, Users } from './scenes';
 
 
 function App() {
   return (
     <Router>
-      <Route path="/" exact component={Home} />
-      <Route path="/api/exercise/new-user" exact component={NewUser} />
+      <Route exact path="/" component={Home} />
+      <Route exact path="/api/exercise/new-user" component={NewUser} />
+      <Route exact path="/api/exercise/users" component={Users} />
     </Router>
   );
 }
